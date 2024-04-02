6ad656bbec97a99698a52db7868107deabbdefd9
AugustoCVS <guty.vertolis@gmail.com>
2024-04-02 12:59:54 -0300

    feat: create routes to newProduct, get and post form

--- src/main/java/api/urbuy/controller/HomeController.java
+++ src/main/java/api/urbuy/controller/HomeController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import api.urbuy.domain.product.Product;
 import api.urbuy.domain.product.ProductRepository;
+import api.urbuy.domain.product.registerProductsData;
 import api.urbuy.domain.purchase.Purchase;
 import api.urbuy.domain.purchase.PurchaseRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -43,6 +47,25 @@ public class HomeController {
         List<Product> products = productRepository.findAll();
         ModelAndView mv = new ModelAndView("products");
         mv.addObject("products", products);
+        return mv;
+    }
+
+    @GetMapping("/newProduct/form")
+    public ModelAndView form(registerProductsData req) {
+        ModelAndView mv = new ModelAndView("newProduct/form");
+        return mv;
+    }
+
+    @PostMapping("/newProduct")
+    public ModelAndView novo(@Valid registerProductsData req, BindingResult result) {
+        ModelAndView mv;
+        if (result.hasErrors()) {
+            mv = new ModelAndView("/newProduct/form");
+            return mv;
+        }
+        Product product = new Product(req);
+        productRepository.save(product);
+        mv = new ModelAndView("redirect:/home");
         return mv;
     }
 
